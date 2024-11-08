ce5394d3bb5b80fb9936d015d0d57e5dc29cf404
byoosi2022 <byoosi2021@gmail.com>
2024-11-08 03:03:41 +0100

    App Initalised

--- customer_portal/custom_api/orders.py
+++ customer_portal/custom_api/orders.py
@@ -39,3 +39,68 @@ def get_customer_sales_orders():
 
     return {'sales_orders': sales_orders}
 
+
+@frappe.whitelist()
+def get_customer_sales_invoices():
+    # Get the currently logged-in user
+    customer_email = frappe.session.user
+    
+    # Fetch customer record using the logged-in user's email
+    customer = frappe.get_all('Customer', filters={'custom_link_user_email': customer_email}, fields=['name'])
+    
+    if not customer:
+        return {'message': _('No customer found for this user.')}
+
+    customer_name = customer[0].name
+
+    # Fetch sales Invoice for the customer
+    sales_invoice = frappe.get_all('Sales Invoice', 
+                                  filters={'customer': customer_name,
+                                           'status': ['!=', 'Cancelled']  # Exclude invoices with status "Cancelled"
+                                           }, 
+                                  fields=['name', 'posting_date', 'grand_total', 'status','outstanding_amount'])
+
+    # Retrieve items 
+    for invoice in sales_invoice:
+        # Fetch items for the invoice
+        items = frappe.get_all('Sales Invoice Item',
+                               filters={'parent': invoice.name},
+                               fields=['item_code', 'item_name', 'qty', 'rate', 'amount'])
+        invoice['items'] = items
+
+   
+
+    return {'sales_invoice': sales_invoice}
+
+
+@frappe.whitelist()
+def get_customer_delivery_notes():
+    # Get the currently logged-in user
+    customer_email = frappe.session.user
+    
+    # Fetch customer record using the logged-in user's email
+    customer = frappe.get_all('Customer', filters={'custom_link_user_email': customer_email}, fields=['name'])
+    
+    if not customer:
+        return {'message': _('No customer found for this user.')}
+
+    customer_name = customer[0].name
+
+    # Fetch sales Invoice for the customer
+    sales_invoice = frappe.get_all('Delivery Note', 
+                                  filters={'customer': customer_name,
+                                           'status': ['!=', 'Cancelled']  # Exclude invoices with status "Cancelled"
+                                           }, 
+                                  fields=['name', 'posting_date', 'grand_total', 'status'])
+
+    # Retrieve items 
+    for invoice in sales_invoice:
+        # Fetch items for the invoice
+        items = frappe.get_all('Delivery Note Item',
+                               filters={'parent': invoice.name},
+                               fields=['item_code', 'item_name', 'qty', 'rate', 'amount'])
+        invoice['items'] = items
+
+   
+
+    return {'sales_invoice': sales_invoice}
